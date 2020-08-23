88336dea126b04217a6a0f5886399981999d47cc
narayanarao <abc@gmail.com>
2020-08-23 22:03:21 +0530

    modified Test.java

--- demogit/src/com/demogit/test/Test.java
+++ demogit/src/com/demogit/test/Test.java
@@ -2,6 +2,10 @@ package com.demogit.test;
 
 public class Test {
 	
+	public void m1() {
+		System.out.println("m1() method");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Main Method Called");
 		System.out.println("Hello World");
